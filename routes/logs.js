a55af8e7ce3b6f13c3971e2107224de7258132fc
Ethan Gallant <ethan.gallant@gmail.com>
2019-11-08 05:54:20 -0500

    Updated logs to be more efficient and removed pointless code

--- routes/logs.js
+++ routes/logs.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const moment = require('moment');
 const {check,param ,validationResult} = require('express-validator');
 const Log = require("../models").Log;
-const Client = require("../models").Client;
 
 
 /* GET all logs of all clients page. */
@@ -51,14 +50,41 @@ router.get('/:clientId', [
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
     }
-    Client.findByPk(req.params.clientId)
-        .then(client => {
-            if (client)
-                res.send(client).end();
-            else
-                res.status(404).send({"err": "log not found in the db."}).end()
-        });
-    console.log("test");
+    Log.findAll({where:{"client_id":req.params.clientId}}).then((data) => {
+        res.send(data).end();
+    }).catch(err=>res.status(500).send({"err":err}).end());
+});
+
+router.get('/:clientId/dtRange',[
+    param("clientId").isInt({ gte: 0}),
+
+    check('start').custom(val=> {
+        return moment(val).isValid()
+    }).exists(),
+    // password must be at least 5 chars long
+    check('end').custom(val=> {
+        return moment(val).isValid()
+    }).optional(),
+], function (req,res,next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    let start_date = moment(req.query.start);
+    let end_date = req.query.end ? moment(req.query.end) : moment(); // if no end date specified use now :)
+
+    Log.findAll({
+        where: {
+            client_id:req.params.clientId,
+            createdAt: { gte: start_date.toISOString(), lte: end_date.toISOString()},
+        }
+    }).then(data=>res.status(200).send({
+        "start_date": start_date.toISOString(),
+        "end_date":end_date.toISOString(),
+        "results":data})
+        .end())
+        .catch(err => res.status(500).send({"error":err}).end());
 });
 
 module.exports = router;
