871a846fcac94c26ae408b489d45d8e815d5b3a7
Ethan Gallant <ethan.gallant@gmail.com>
2019-11-11 21:00:21 -0500

    Clean up our socket code and use the new ClientRequest model

--- socket/index.js
+++ socket/index.js
@@ -1,19 +1,69 @@
-const time_to_auth = 10;
+const Client = require("../models").Client;
+const ClientRequest = require("../models").ClientRequest;
+const moment = require('moment');
 
-module.exports = (io)=>{
+module.exports = (io) => {
 
+    io.on('connection', (socket) => {
+        console.log("Client connected");
+        socket.on('disconnect', function () {
+            console.log(JSON.stringify(socket.db_client));
+            if(socket.db_client){
+                socket.db_client.update({
+                    connectedAt: moment(),
+                    connected:false
+                });
+            }
+            console.log('Got disconnect!');
+        });
 
-    io.on('connection',(socket)=>{
-      socket.emit("please-authenticate");
+        socket.on('log', function (log) {
+            console.log("================DATA LOG SENT====================");
+            console.log(JSON.stringify(log));
+            console.log("=================================================");
+        });
+        socket.on('login', (data) => {
+            console.log("Auth Data Requested: " + JSON.stringify(data));
+            Client.findAll({
+                limit: 1,
+                where: {
+                    serial: data.serial
+                }
+            }).then(function (entries) {
+                if (entries.length <= 0) {
+                    ClientRequest.findAll({
+                        limit: 1,
+                        where: {
+                            mac: data.serial
+                        }
+                    }).then(function (requestEntries) {
+                        if (requestEntries.length <= 0) {
+                            ClientRequest.create({mac: data.serial});
+                        }
+                    });
+                }
+                if (entries.length >= 1) {
+                    socket.emit("login-success", true);
+                    socket.db_client = entries[0];
+                    socket.db_client.update({
+                        connectedAt: moment(),
+                        connected:true
+                    });
+                    console.log("USER LOGIN SUCCESS")
+                }
+                else {
+                    socket.emit("login-success", false);
+                    console.log("USER LOGIN FAILED. INVALID SERIAL");
+                }
+            });
+
+        });
+        socket.on('stats', function (data) {
+            console.log("DATA GIVEN VIA STATS" + JSON.stringify(data));
+        });
+
 
     });
-    io.on('authenticate',(data)=>{
-        const serial = data.serial;
-
-    });
-
-
-};
-
+}
 
 
